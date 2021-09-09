4a68fca736b8e3afba40772e67d501515d4441cc
Fabien Dimitrov <fdimitrov@coveo.com>
2021-09-09 01:55:02 -0400

    Add setPlayerMoves()

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,25 @@
 // All code should be written in this file.
+
+// Player 1's moves (you)
+const playerOneMoves = [];
+
+// Player 2's moves (other human or computer)
+const playerTwoMoves = [];
+
+// Sets player moves 
+// Here, the 'moves' parameter is a 2D array consisting of 1] The move type (Rock, Paper, Scissors) and 2] The move value (0-99 strength points) aka the 'twist'
+const setPlayerMoves = (player, moves) => {
+
+    if (player == "Player One") {
+        
+        playerOneMoves.push(moves)
+    }
+    else if (player == "Player Two") {
+
+        playerTwoMoves.push(moves)
+    }
+    else {
+        throw "Invalid player selection. Must be either \"Player One\" or \"Player Two\"."
+    }
+}
+
